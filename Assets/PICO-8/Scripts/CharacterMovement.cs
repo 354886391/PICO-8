4caf6e1a175a5f81fc0d8cd5360861493e7cb4be
Ancolo <miogo@sina.com>
2021-07-27 18:11:09 +0800

    0727_1810

---
+++ Assets/PICO-8/Scripts/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public void Move()
+    {
+        //DetectGround();
+        //ApplyMovement();
+        //SnapToGround();
+        //LimitLateralVelocity();
+        //LimitVerticalVelocity();
+        //PreventGroundPenetration();
+    }
+
+    private void ApplyMovement(Vector3 desiredVelocity, float maxDesiredSpeed)
+    {
+
+    }
+}
---
+++ Assets/PICO-8/Scripts/Console.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public static class Console
+{
+    [System.Diagnostics.Conditional("ENABLE_DEBUG")]
+    public static void Log(object message)
+    {
+        UnityEngine.Debug.Log(message);
+    }
+
+    [System.Diagnostics.Conditional("ENABLE_DEBUG")]
+    public static void LogWarning(object message)
+    {
+        UnityEngine.Debug.LogWarning(message);
+    }
+
+    [System.Diagnostics.Conditional("ENABLE_DEBUG")]
+    public static void LogError(object message)
+    {
+        UnityEngine.Debug.LogError(message);
+    }
+
+    [System.Diagnostics.Conditional("ENABLE_DEBUG")]
+    public static void LogFormat(string format, params object[] args)
+    {
+        UnityEngine.Debug.LogFormat(format, args);
+    }
+
+    [System.Diagnostics.Conditional("ENABLE_DEBUG")]
+    public static void DrawRay(Vector2 start, Vector2 end, Color color)
+    {
+        UnityEngine.Debug.DrawRay(start, end, color);
+    }
+}
+

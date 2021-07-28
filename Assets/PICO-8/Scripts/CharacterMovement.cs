440c2290365fa2dd8fcab94ba59e395f3de604c1
Ancolo <miogo@sina.com>
2021-07-28 20:45:29 +0800

    0728_2045

--- Assets/PICO-8/Scripts/CharacterMovement.cs
+++ Assets/PICO-8/Scripts/CharacterMovement.cs
@@ -5,7 +5,73 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
+    #region Structs
 
+    private struct RayOrigin
+    {
+        public Vector2 topLeft;
+        public Vector2 bottomLeft;
+        public Vector2 bottomRight;
+    }
+    #endregion
+
+    #region Constants
+
+    private const float MaxRun = 9f;
+    private const float MaxFall = -16f;
+    private const float FastMaxFall = -24f;
+    private const float FastMaxAccel = 30f;
+    private const float RunAccel = 50f;
+    private const float RunReduce = 40f;
+
+    private const float JumpHBoost = 7.2f;
+    private const float JumpSpeed = 13.5f;
+    private const float JumpTime = 0.2f;
+
+    private const float Gravity = 90f;
+    private const float HalfGravThreshold = 4f;
+
+    private const float AirMult = 0.65f;
+
+    private const float DashSpeed = 25.6f;
+    private const float EndDashSpeed = 12.8f;
+    private const float EndDashUpMult = .75f;
+    private const float DashTime = 0.15f;
+    private const float DashCooldown = 0.2f;
+    private const float DashRefillCooldown = 0.1f;
+    private const int DashHJumpThruNudge = 6;
+    private const int DashCornerCorrection = 4;
+    private const int DashVFloorSnapDist = 3;
+    private const float DashAttackTime = 0.3f;
+
+    private const float ClimbSpeed = 4.8f;
+    private const float ClimbSlipSpeed = -3.6f;
+    private const float ClimbAccel = Gravity + 50f;
+    private const float ClimbGrabYMult = .2f;
+    #endregion
+
+    #region Vars
+
+    [SerializeField] private bool _onGround; //当前帧在地面    
+
+    [SerializeField] private int _facing = 1;
+    [SerializeField] private int _jumpCount;
+    [SerializeField] private float _maxFall;
+    [SerializeField] private Vector2 _speed;
+
+    [SerializeField] private float _skinWidth = 0.02f;
+    [SerializeField] private float _verticalRaysCount;
+    [SerializeField] private float _horizontalRaysCount;
+    [SerializeField] private float _verticalBetweenRays;
+    [SerializeField] private float _horizontalBetweenRays;
+
+    private RayOrigin _rayOrigin;
+
+    [SerializeField] private LayerMask _platformMask;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private BoxCollider2D _boxCollider;
+    [SerializeField] private RaycastHit2D _raycastHit;
+    #endregion
 
     public void Move()
     {
@@ -28,8 +94,27 @@ public class CharacterMovement : MonoBehaviour
 
     }
 
-    private void ApplyMovement(Vector3 desiredVelocity, float maxDesiredSpeed)
+    private void ApplyGravity(float deltaTime)
     {
+        if (!_onGround)
+        {
+            float max = MaxFall;
+            float mult = Mathf.Abs(_speed.y) < HalfGravThreshold && Input.Jump ? 0.5f : 1.0f;
+            _speed.y = Mathf.MoveTowards(_speed.y, max, Gravity * mult * deltaTime);
+        }
+    }
 
+    private void ApplyMovement(float moveX, float deltaTime)
+    {
+        float mult = _onGround ? 1.0f : AirMult;
+        if (Mathf.Abs(_speed.x) >= MaxRun && Mathf.Sign(_speed.x) == moveX)
+        {
+            _speed.x = Mathf.MoveTowards(_speed.x, MaxRun * moveX, RunReduce * mult * deltaTime);  //Reduce back from beyond the max speed
+        }
+        else
+        {
+            _speed.x = Mathf.MoveTowards(_speed.x, MaxRun * moveX, RunAccel * mult * deltaTime);   //Approach the max speed          
+        }
+        Console.LogFormat("Move speed X {0:F3}", _speed.x);
     }
 }
---
+++ Assets/PICO-8/Scripts/InputController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public float MoveX;
+    public float MoveY;
+    public bool Jump;
+
+
+    private void HandleInput()
+    {
+        MoveX = Input.GetAxisRaw("Horizontal");
+        MoveY = Input.GetAxisRaw("Vertical");
+    }
+
+    protected class JumpCheck
+    {
+        protected bool _canJump = true;
+        protected bool _jump;
+        protected bool _isJumping;
+        protected bool _updateJumpTimer;
+        protected float _jumpTimer;
+        protected float _jumpHeldDownTimer;
+        protected float _jumpToleranceTime = 0.15f;
+        protected float _extraJumpTime = 0.5f;
+        protected float _extraJumpPower = 25.0f;
+        protected int _midAirJumpCount;
+        protected int _maxMidAirJumps = 1;
+
+
+        public bool Jump
+        {
+            get { return _jump; }
+            set
+            {
+                // If jump is released, allow to jump again
+                if (_jump && value == false)
+                {
+                    _canJump = true;
+                    _jumpHeldDownTimer = 0.0f;
+                }
+                // Update jump value; if pressed, update held down timer
+                _jump = value;
+                if (_jump)
+                {
+                    _jumpHeldDownTimer += Time.deltaTime;
+                }
+            }
+        }
+
+        public bool IsJumping
+        {
+            get
+            {
+                if (_isJumping && speed.y < 0.0001f)
+                {
+                    _isJumping = false;
+                }
+                return _isJumping;
+            }
+        }
+
+        public bool IsFalling
+        {
+            get { return !onGround && speed.y < 0.0001f; }
+        }
+
+
+        protected void Jumping()
+        {
+            if (!_jump || !_canJump) return;
+            if (_jumpHeldDownTimer > _jumpToleranceTime) return;
+            _canJump = false;
+            _isJumping = true;
+            _updateJumpTimer = true;
+            // Apply jump impulse
+
+        }
+
+        protected void MidAirJumping()
+        {
+            if (_midAirJumpCount > 0 && onGround)
+            {
+                _midAirJumpCount = 0;
+            }
+            if (!_jump || !_canJump) return;
+            if (_midAirJumpCount >= _maxMidAirJumps) return;
+            _midAirJumpCount++;
+            _canJump = false;
+            _isJumping = true;
+            _updateJumpTimer = true;
+            // Apply jump impulse
+
+        }
+
+        protected void UpdateJumpTimer()
+        {
+            if (!_updateJumpTimer) return;
+            if (_jump && _jumpTimer < _extraJumpTime)
+            {
+                var jumpProcess = _jumpTimer / _extraJumpTime;
+                var proportionaljumpPower = Mathf.Lerp(_extraJumpPower, 0.0f, jumpProcess);
+                //movement.(Vector3.up * proportionalJumpPower, ForceMode.Acceleration);
+                _jumpTimer = Mathf.Min(_jumpTimer + Time.deltaTime, _extraJumpTime);
+            }
+            else
+            {
+                _jumpTimer = 0.0f;
+                _updateJumpTimer = false;
+            }
+        }
+
+        protected void Move()
+        {
+            //movement.Move();
+            Jumping();
+            MidAirJumping();
+            UpdateJumpTimer();
+        }
+
+    }
+}

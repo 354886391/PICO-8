480abda2eb9f70476c71ed2b5bccc014905d47e3
Ancolo <miogo@sina.com>
2021-09-07 16:05:18 +0800

    0907_1605

--- Assets/PICO-8/Scripts/CameraFollow.cs
+++ Assets/PICO-8/Scripts/CameraFollow.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Transform _targetTransform;
+    [SerializeField]
+    private float _followSpeed = 3.0f;
+
+    public void Awake()
     {
-        
+        GetScreenSize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private float _boundWidth;
+    private float _boundHeight;
+
+    private void GetScreenSize()
     {
-        
+        var displayMain = Display.main;
+        var renderingRate = displayMain.renderingWidth / displayMain.renderingHeight;
+        var cameraHeight = Camera.main.orthographicSize;
+        var cameraWidth = cameraHeight * renderingRate;
+        _boundWidth = cameraWidth * 0.618f;
+        _boundHeight = cameraHeight * 0.618f;
+        Console.LogFormat("Width {0}, height {1}", cameraWidth, cameraHeight);
+    }
+
+    private void ComputeTargetPosition()
+    {
+        var hDistance = transform.position.x - _targetTransform.position.x;
+        var vDistance = transform.position.y - _targetTransform.position.y;
+        Console.LogFormat("HDistance {0}, VDistance {1} ", hDistance, vDistance);
+        var targetPosition = Vector3.zero;
+        if (hDistance > _boundWidth)
+        {
+            targetPosition.x = Mathf.Lerp(targetPosition.x, hDistance, _followSpeed * Time.deltaTime);
+        }
+        if (vDistance > _boundHeight)
+        {
+            targetPosition.y = Mathf.Lerp(targetPosition.y, vDistance, _followSpeed * Time.deltaTime);
+        }
+        targetPosition.z = -50f;
+        transform.position = targetPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Console.LogFormat("OnTriggerEnter2D {0}", collision.name);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Console.LogFormat("OnTriggerStay2D {0}", collision.name);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Console.LogFormat("OnTriggerExit2D {0}", collision.name);
+    }
+
+
+    public void LateUpdate()
+    {
+        ComputeTargetPosition();
     }
 }

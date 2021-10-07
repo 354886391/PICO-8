da44d5cc7d5daaeaf30393a13f2962b2b7e844f6
Ancolo <miogo@sina.com>
2021-10-07 18:33:09 +0800

    1007_1833

--- Assets/PICO-8/Scripts/Base/CharacterHairFlow.cs
+++ Assets/PICO-8/Scripts/Base/CharacterHairFlow.cs
@@ -1,4 +1,5 @@
 ﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,19 +8,17 @@ public class CharacterHairFlow : MonoBehaviour
     public Transform TargetTrans;   // player
     private int _positionCount = 6;
     private int _hairRendererCount = 6;
-    private List<Vector3> _positionList;    // 6
+    private List<Vector3> _positionList = new List<Vector3>();    // 6
     [SerializeField]
-    private SpriteRenderer[] _hairRenderers;    // 7
+    private List<SpriteRenderer> _hairRenderers = new List<SpriteRenderer>();    // 6
 
     private Color _normalRed = new Color(1f, 0f, 77 / 255f);
     private Color _dashBlue = new Color(41 / 255f, 173 / 255f, 1f);
 
     private void Start()
     {
-        _positionList = new List<Vector3>();
         for (int i = 0; i < _positionCount; i++)
             _positionList.Add(TargetTrans.localPosition);
-        //_hairRenderers = GetComponentsInChildren<SpriteRenderer>();   //包含父Renderer
         AddMovementEvent();
     }
 
@@ -30,28 +29,34 @@ public class CharacterHairFlow : MonoBehaviour
         CharacterMovement.LandingEvent += LandingHandler;
     }
 
+    public void ResetPlace()
+    {
+
+        foreach (var item in _hairRenderers)
+            item.transform.localPosition = TargetTrans.localPosition;
+    }
+
     public void UpdateHairFlow(CharacterMovement movement)
     {
         _positionList.RemoveAt(0);
-        var targetPosition = TargetTrans.localPosition;
-        _positionList.Add(targetPosition);
+        _positionList.Add(TargetTrans.localPosition);
         for (int i = 0; i < _hairRendererCount; i++)
         {
             _hairRenderers[i].transform.localScale = new Vector3(-movement.Facing, 1, 1);
             _hairRenderers[i].transform.localPosition = _positionList[_positionCount - 1 - i] + new Vector3(-movement.Facing * 0.5f, -0.125f, -0.1f);
         }
-        if (movement.Speed == Vector2.zero)
-        {
-            ResetPlace();
-        }
     }
 
-    public void ResetPlace()
+    private int index = 0;
+    public IEnumerator UpdateHairFlow2(CharacterMovement movement)
     {
-        foreach (var item in _hairRenderers)
-        {
-            item.transform.localPosition = TargetTrans.localPosition;
-        }
+        if (index == 6) index = 0;  // 更新到结尾时重置索引
+        _positionList[5 - index] = TargetTrans.localPosition;
+        _hairRenderers[index].transform.localScale = new Vector3(-movement.Facing, 1, 1);
+        _hairRenderers[index].transform.localPosition = _positionList[index];
+        ++index;
+        yield return null;
+
     }
 
     private void SetHairColor(Color color)
@@ -62,19 +67,49 @@ public class CharacterHairFlow : MonoBehaviour
         }
     }
 
-    private void DashBeginHandler(CharacterMovement obj)
+    private void DashBeginHandler(CharacterMovement movement)
     {
         SetHairColor(_dashBlue);
     }
 
-    private void DashEndHandler(CharacterMovement obj)
+    private void DashEndHandler(CharacterMovement movement)
     {
-        if (obj.OnGround) SetHairColor(_normalRed);
+        if (movement.OnGround) SetHairColor(_normalRed);
     }
 
-    private void LandingHandler(CharacterMovement obj)
+    private void LandingHandler(CharacterMovement movement)
     {
         SetHairColor(_normalRed);
     }
 }
 
+public class player_hair
+{
+    private class node
+    {
+        public float x;
+        public float y;
+        public float size;
+    }
+
+    private node[] hair = new node[5];
+
+    public player_hair(CharacterMovement obj)
+    {
+        for (var i = 0; i <= 4; i++)
+            hair[i] = new node() { x = obj.Speed.x, y = obj.Speed.y, size = E.max(1, E.min(2, 3 - i)) };
+    }
+
+    public void draw_hair(CharacterMovement obj, int facing, int djump)
+    {
+        var c = (djump == 1 ? 8 : (djump == 2 ? (7 + E.flr((G.frames / 3) % 2) * 4) : 12));
+        var last = new Vector2(obj.x + 4 - facing * 2, obj.y + (E.btn(G.k_down) ? 4 : 3));
+        foreach (var h in hair)
+        {
+            h.x += (last.x - h.x) / 1.5f;
+            h.y += (last.y + 0.5f - h.y) / 1.5f;
+            E.circfill(h.x, h.y, h.size, c);
+            last = new Vector2(h.x, h.y);
+        }
+    }
+}

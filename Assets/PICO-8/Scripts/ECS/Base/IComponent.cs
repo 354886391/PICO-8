bbcdac8696486d1bda900a16479e7da3cdc85886
Ancolo <miogo@sina.com>
2021-10-14 18:20:36 +0800

    1014_1820

---
+++ Assets/PICO-8/Scripts/ECS/Base/IComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class IComponent
+{
+    public virtual void AddComponent() { }
+
+    public virtual Icomonent GetComponent<Icomonent>() { return default; }
+
+    public virtual bool HasComponent<Icomonent>() { return false; }
+}

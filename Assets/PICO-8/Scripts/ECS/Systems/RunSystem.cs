0938d9d2044f1aa37d01612a48fe95dc82701786
Ancolo <miogo@sina.com>
2021-10-20 01:16:11 +0800

    1020_0116

--- Assets/PICO-8/Scripts/ECS/Systems/RunSystem.cs
+++ Assets/PICO-8/Scripts/ECS/Systems/RunSystem.cs
@@ -12,20 +12,17 @@ public class RunSystem : ISystem
         throw new System.NotImplementedException();
     }
 
-    private void Run(ref RunComponent run, in InputComponent input, float deltaTime)
+    private void Run(StateComponent state, in InputComponent input, float deltaTime)
     {
-        var maxRun = Constants.MaxRun;
-        var runAccel = Constants.RunAccel;
-        var runReduce = Constants.RunReduce;
-        var mult = run.OnGround ? 1.0f : 0.6f;
+        var mult = state.OnGround ? 1.0f : 0.6f;
         // 当X轴速度超过最大速度且速度方向与移动方向一致时减速到最大速度
-        if (Mathf.Abs(run.Speed.x) > maxRun && Mathf.Sign(run.Speed.x) == input.MoveX)
+        if (Mathf.Abs(state.Speed.x) > Constants.MaxRun && Mathf.Sign(state.Speed.x) == input.MoveX)
         {
-            run.Speed.x = ECSUtility.Approach(run.Speed.x, maxRun * input.MoveX, runReduce * mult * deltaTime);
+            state.Speed.x = ECSUtility.Approach(state.Speed.x, Constants.MaxRun * input.MoveX, Constants.RunReduce * mult * deltaTime);
         }
         else
         {
-            run.Speed.x = ECSUtility.Approach(run.Speed.x, maxRun * input.MoveX, runAccel * mult * deltaTime);
+            state.Speed.x = ECSUtility.Approach(state.Speed.x, Constants.MaxRun * input.MoveX, Constants.RunAccel * mult * deltaTime);
         }
     }
 }

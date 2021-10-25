a0fa6f50211322d24c69bffb322c9a680d2b2cf6
Ancolo <miogo@sina.com>
2021-10-26 01:26:44 +0800

    1026_0126

--- Assets/InstanceTest.cs
+++ Assets/InstanceTest.cs
@@ -9,18 +9,19 @@ public class InstanceTest : MonoBehaviour
     {
 
         Debug.LogFormat("a {0}", abc.a);
-        abc.a = 2;
+        abc.a += 1;
         Debug.LogFormat("abc.a {0}", abc.a);
         PlusA(abc);
         Debug.LogFormat("PlusA {0}", abc.a);
         abc.PlusA(abc);
-        Debug.LogFormat("abc.PlusA {0}", abc.a);
+        Debug.LogFormat("abc.PlusA {0}", abc.a);        
+        abc.DebugA();
 
     }
 
     private void PlusA(Abc abc)
     {
-        abc.a = 3;
+        abc.a += 1;
     }
 }
 
@@ -30,11 +31,12 @@ public class Abc
 
     public void PlusA(Abc abc)
     {
-        abc.a = 4;
+        abc.a +=1;
     }
 
-    public void DebugA(Abc abc)
+    public void DebugA()
     {
-        Debug.LogFormat("Abc.PlusA {0}", abc.a);
+        a += 1;
+        Debug.LogFormat("Abc.PlusA {0}", a);
     }
 }

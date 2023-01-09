e7c12b8a4d53ab12ee5c23712098b76a390ca596
Ancolo <miogo@sina.com>
2023-01-10 00:52:34 +0800

    0110_0052

--- Assets/Scripts/Normal/Character/CharacterController.cs
+++ Assets/Scripts/Normal/Character/CharacterController.cs
@@ -1,8 +1,24 @@
 ﻿#define NO_ENABLE_DEBUG
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class CharacterController : MonoBehaviour
 {
+    public const int StNormal = 0;
+    public const int StClimb = 1;
+    public const int StDash = 2;
+    public const int StSwim = 3;
+    public const int StBoost = 4;
+    public const int StRedDash = 5;
+    public const int StHitSquash = 6;
+    public const int StLaunch = 7;
+    public const int StPickup = 8;
+    public const int StDreamDash = 9;
+    public const int StReflectionFall = 18;
+    public const int StStarFly = 19;
+    public const int StTempleFall = 20;
+    public const int StCassetteFly = 21;
+    public const int StAttract = 22;
 
     [SerializeField]
     public CharacterInput _input;
@@ -11,6 +27,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     public CharacterCameraFollow _cameraFollow;
 
+    public StateMachine _machine;
+
     private void Awake()
     {
         Application.targetFrameRate = 60;
@@ -18,16 +36,38 @@ public class CharacterController : MonoBehaviour
 
     private void Start()
     {
+        _machine = new StateMachine(15);
+        _machine.SetCallbacks(StNormal, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StClimb, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StDash, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StSwim, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StBoost, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StRedDash, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StHitSquash, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StLaunch, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StPickup, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StDreamDash, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StReflectionFall, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StStarFly, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StTempleFall, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StCassetteFly, update: null, coroutine: null, begin: null, end: null);
+        _machine.SetCallbacks(StAttract, update: null, coroutine: null, begin: null, end: null);
 
     }
 
-    private void Update()
+    private void NormalBegin()
     {
-        _movement.Move(_input, Time.deltaTime);    // 左右移动
+
     }
 
-    private void LateUpdate()
+    private void NormalEnd()
     {
-        _cameraFollow.UpdateFollow(Time.deltaTime);
+
+    }
+
+    private int NormalUpdate()
+    {
+
+        return StNormal;
     }
 }
